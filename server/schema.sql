aab271007358c9b81918a1f58d076f25779f84bc
ryanjlee <ryanjlee12@gmail.com>
2015-01-09 17:04:56 -0800

    schema.sql configured to create users, rooms, and messages tables joined by foreign keys.

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id int(11) NOT NULL auto_increment,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id int(11) NOT NULL auto_increment,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int(11) NOT NULL auto_increment,
+  text VARCHAR(255),
+  user_id int(11),
+  room_id int(11),
+  PRIMARY KEY (id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
